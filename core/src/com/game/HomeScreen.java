0978906feb71ba4200439f4cf5bfb994b7c56bc4
Henry Yuen <henry42385@gmail.com>
2022-11-14 19:11:02 +1100

    Home screen background

--- core/src/com/game/HomeScreen.java
+++ core/src/com/game/HomeScreen.java
@@ -13,17 +13,10 @@ public class HomeScreen extends Screen {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private Texture img;
-    private Rectangle bucket;
     private BitmapFont font;
 
     public void create() {
         img = new Texture("sprites/Water.png");
-
-        bucket = new Rectangle();
-        bucket.x = 800 / 2 - 64 / 2;
-        bucket.y = 20;
-        bucket.width = 64;
-        bucket.height = 64;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
@@ -37,29 +30,38 @@ public class HomeScreen extends Screen {
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
-        batch.draw(img, bucket.x, bucket.y, 400, 400);
-        if (Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            bucket.x = touchPos.x - 64 / 2;
+        for (int i = 0; i < Gdx.graphics.getWidth(); i += 128) {
+            for (int j = 0; j < Gdx.graphics.getHeight(); j += 128) {
+                batch.draw(img, i, j, 128, 128);
+            }
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        System.out.println(Gdx.graphics.getWidth()/2);
+        System.out.println(Gdx.input.getX());
+        font.draw(batch, "Click play to start", 540, 360);
 
-        if (bucket.x < 0) bucket.x = 0;
-        if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+//        if (Gdx.input.isTouched()) {
+//            Vector3 touchPos = new Vector3();
+//            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+//            camera.unproject(touchPos);
+//        }
+
+//        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+//            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+//        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+//            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+//
+//        if (bucket.x < 0) bucket.x = 0;
+//        if (bucket.x > 800 - 64) bucket.x = 800 - 64;
 
 
-        font.draw(batch, "Click play to start", 200, 200);
+
         batch.end();
     }
 
     public void dispose() {
         batch.dispose();
         img.dispose();
+        font.dispose();
     }
 }
