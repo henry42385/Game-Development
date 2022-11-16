a812c3e4dfaff85bfabbd1031d43260457ad1cff
Henry Yuen <henry42385@gmail.com>
2022-11-16 20:38:32 +1100

    snap to grid

--- core/src/com/game/GameScreen.java
+++ core/src/com/game/GameScreen.java
@@ -8,25 +8,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 public class GameScreen extends Screen {
     private Texture spriteSheet;
-    private Texture cloud;
+    private Texture target;
     private SpriteBatch batchStatic;
     private SpriteBatch batchDynamic;
     private ShapeRenderer shapeRenderer;
     private Map map;
     private BitmapFont font;
     private boolean playerTurn1 = true;
+    private Vector3 mouseLocation;
 
     public void create() {
         spriteSheet = new Texture("sprites/TerrainSpritesheetOld.png");
-        cloud = new Texture("sprites/MissileShip.png");
+        target = new Texture("sprites/Target.png");
         batchStatic = new SpriteBatch();
         batchDynamic = new SpriteBatch();
         map = new Map("assets/maps/map1.txt");
         shapeRenderer = new ShapeRenderer();
         font = new BitmapFont();
+        mouseLocation = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
 
         Gdx.input.setInputProcessor(new InputAdapter() {
 
@@ -42,10 +45,11 @@ public class GameScreen extends Screen {
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        mouseLocation.x = Gdx.input.getX();
+        mouseLocation.y = Gdx.input.getY();
         renderWorld();
         renderHUD();
         updateView();
-        System.out.println(playerTurn1);
     }
 
     private void renderWorld() {
@@ -53,21 +57,25 @@ public class GameScreen extends Screen {
         batchDynamic.setProjectionMatrix(DynamicCamera.get().combined);
         batchDynamic.begin();
 
-//        Vector3 mouseLocation = StaticCamera.get().unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+        Vector3 worldMouseLocation = DynamicCamera.get().unproject(mouseLocation);
+//        System.out.println(worldMouseLocation.x);
+        System.out.println(worldMouseLocation.y);
+//        System.out.println((int)(worldMouseLocation.x / 128) * 128);
 
         char[][] drawMap = map.getMap();
         for (int y = 0; y < map.getHeight(); y++) {
             for (int x = 0; x < map.getWidth(); x++) {
                 if (drawMap[y][x] == 'D')
-                    batchDynamic.draw(spriteSheet, x * 128 - 256, (map.getHeight() - y - 1) * 128 - 256, 128, 128, 0, 0.5f, 0.5f, 0);
+                    batchDynamic.draw(spriteSheet, x * 128, (map.getHeight() - y - 1) * 128, 128, 128, 0, 0.5f, 0.5f, 0);
                 else if (drawMap[y][x] == 'S')
-                    batchDynamic.draw(spriteSheet, x * 128 - 256, (map.getHeight() - y - 1) * 128 - 256, 128, 128, 0.5f, 0.5f, 1, 0);
+                    batchDynamic.draw(spriteSheet, x * 128, (map.getHeight() - y - 1) * 128, 128, 128, 0.5f, 0.5f, 1, 0);
                 else if (drawMap[y][x] == 'L')
-                    batchDynamic.draw(spriteSheet, x * 128 - 256, (map.getHeight() - y - 1) * 128 - 256, 128, 128, 0, 1, 0.5f, 0.5f);
+                    batchDynamic.draw(spriteSheet, x * 128, (map.getHeight() - y - 1) * 128, 128, 128, 0, 1, 0.5f, 0.5f);
                 else if (drawMap[y][x] == 'M')
-                    batchDynamic.draw(spriteSheet, x * 128 - 256, (map.getHeight() - y - 1) * 128 - 256, 128, 128, 0.5f, 1, 1, 0.5f);
+                    batchDynamic.draw(spriteSheet, x * 128, (map.getHeight() - y - 1) * 128, 128, 128, 0.5f, 1, 1, 0.5f);
             }
         }
+        batchDynamic.draw(target, (int)(worldMouseLocation.x / 128) * 128 + 32, (int)(worldMouseLocation.y / 128) * 128 + 32, 64, 64);
 
         batchDynamic.end();
 
@@ -76,10 +84,10 @@ public class GameScreen extends Screen {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(0, 0, 0, 1);
         for (int i = 1; i < map.getHeight() - 2; i++) {
-            shapeRenderer.line(0, -128 + i * 128, map.getWidth() * 128 - 512, -128 + i * 128);
+            shapeRenderer.line(256, 128 + i * 128, map.getWidth() * 128 - 256, 128 + i * 128);
         }
         for (int i = 1; i < map.getWidth() - 2; i++) {
-            shapeRenderer.line(-128 + i * 128, 0, -128 + i * 128, map.getHeight() * 128 - 512);
+            shapeRenderer.line(128 + i * 128, 256, 128 + i * 128, map.getHeight() * 128 - 256);
         }
         shapeRenderer.end();
     }
@@ -87,7 +95,6 @@ public class GameScreen extends Screen {
     private void renderHUD() {
         StaticCamera.update();
         batchStatic.begin();
-        batchStatic.draw(cloud, 450, 450, 200, 200);
         font.getData().setScale(2);
         if (playerTurn1) {
             font.draw(batchStatic, "Player 2",1300, 750);
